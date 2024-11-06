2a350044b733a30dd91ed88a3116fbce0da974cc
Naman065Malik <naman065op@gmail.com>
2024-11-06 14:37:11 +0530

    Password Tag added in Register Form

--- Frontend/src/Components/Pages/LoginPage/RegisterFormStep1.jsx
+++ Frontend/src/Components/Pages/LoginPage/RegisterFormStep1.jsx
@@ -14,7 +14,7 @@ function RegisterFormStep1({setFormData,data}){
             </div>
             <div className="mt-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="text" onChange={(e) => handleForm("password", e.target.value)} value={data?.password || ''}/>
+                <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" onChange={(e) => handleForm("password", e.target.value)} value={data?.password || ''}/>
             </div>
         </>
     )
