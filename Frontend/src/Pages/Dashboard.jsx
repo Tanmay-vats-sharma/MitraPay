31cfaea6c8ca10cf5b65c8ed4203a1c4dd43fd4b
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-11-11 03:03:02 +0530

    Balance card fixed

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Navbar1, Navbar2, Navbar3 } from '../Components/Common/Navbar'
+import React from "react";
+import { Navbar1, Navbar2, Navbar3 } from "../Components/Common/Navbar";
+import profile_pic from "../assets/mohitgoel.jpeg"
 
 function Dashboard() {
   return (
@@ -8,117 +9,85 @@ function Dashboard() {
         {/* Side Navigation Bar */}
         <Navbar2></Navbar2>
         <div className="flex-grow h-screen text-gray-800 ml-0 sm:ml-0 ">
-          <header className="flex items-center h-16 px-6 sm:px-10 bg-white">
-            <button className="block sm:hidden relative flex-shrink-0 p-2 mr-2 text-gray-600 hover:bg-gray-100 hover:text-gray-800 focus:bg-gray-100 focus:text-gray-800 rounded-full">
-              <span className="sr-only">Menu</span>
-              <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
-              </svg>
-            </button>
-            <div className="relative w-full max-w-md sm:-ml-2">
-              <svg aria-hidden="true" viewBox="0 0 20 20" fill="currentColor" className="absolute h-6 w-6 mt-2.5 ml-2 text-gray-400">
-                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-              </svg>
-              <input type="text" role="search" placeholder="Search..." className="py-2 pl-10 pr-4 w-full border-4 border-transparent placeholder-gray-400 focus:bg-gray-50 rounded-lg" />
-            </div>
-            <div className="flex flex-shrink-0 items-center ml-auto">
-              <button className="inline-flex items-center p-2 hover:bg-gray-100 focus:bg-gray-100 rounded-lg">
-                <span className="sr-only">User Menu</span>
-                <div className="hidden md:flex md:flex-col md:items-end md:leading-tight">
-                  <span className="font-semibold">Grace Simmons</span>
-                  <span className="text-sm text-gray-600">Lecturer</span>
-                </div>
-                <span className="h-12 w-12 ml-2 sm:ml-3 mr-2 bg-gray-100 rounded-full overflow-hidden">
-                  <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="user profile photo" className="h-full w-full object-cover" />
-                </span>
-                <svg aria-hidden="true" viewBox="0 0 20 20" fill="currentColor" className="hidden sm:block h-6 w-6 text-gray-300">
-                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                </svg>
-              </button>
-              <div className="border-l pl-3 ml-3 space-x-1">
-                <button className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
-                  <span className="sr-only">Notifications</span>
-                  <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-red-500 rounded-full"></span>
-                  <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-red-500 rounded-full animate-ping"></span>
-                  <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                  </svg>
-                </button>
-                <button className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
-                  <span className="sr-only">Log out</span>
-                  <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </header>
           <div className="h-[calc(100%-4rem)] flex flex-col justify-evenly">
             <div className="flex justify-between items-center h-1/6 px-8">
               <div className="mr-6">
                 <h1 className="text-4xl font-semibold mb-2">Dashboard</h1>
-                <h2 className="text-gray-600 ml-0.5">Mobile UX/UI Design course</h2>
               </div>
               <div className="flex flex-wrap items-start justify-end -mb-3">
                 <button className="inline-flex px-5 py-3 text-purple-600 hover:text-purple-700 focus:text-purple-700 hover:bg-purple-100 focus:bg-purple-100 border border-purple-600 rounded-md mb-3">
-                  <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-5 w-5 -ml-1 mt-0.5 mr-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                  <svg
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="flex-shrink-0 h-5 w-5 -ml-1 mt-0.5 mr-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                    />
                   </svg>
                   Manage dashboard
                 </button>
                 <button className="inline-flex px-5 py-3 text-white bg-purple-600 hover:bg-purple-700 focus:bg-purple-700 rounded-md ml-6 mb-3">
-                  <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 h-6 w-6 text-white -ml-1 mr-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                  <svg
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="flex-shrink-0 h-6 w-6 text-white -ml-1 mr-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
                   </svg>
                   Create new dashboard
                 </button>
               </div>
             </div>
-            <div className='h-5/6 flex '>
-              <div className='h-full w-1/2 flex justify-center items-center border-green-500 border-4 '>
-                <div className='h-[90%] bg-white w-4/6 flex flex-col justify-evenly items-center'>
-                  <div className='h-[20%] w-full flex flex-col items-center justify-center space-y-2  '>
-                    <div className='h-[100px] w-[100px] overflow-hidden rounded-full border-purple-500 border-2'>
-                      <img src="" alt="" />
+            <div className="h-5/6 flex ">
+              <div className="h-full w-1/2 flex justify-center items-center  ">
+                <div className="h-[95%] bg-white w-[60%] flex flex-col justify-evenly items-center">
+                  <div className="h-[24%] w-full flex flex-col items-center justify-center space-y-2  ">
+                    <div className="h-[100%] w-[21%] overflow-hidden rounded-full  ">
+                      <img src={profile_pic} alt="profile_pic" />
                     </div>
-                    <div className='text-xl font-semibold'>Hi, Naman Malik</div>
+                    <div className="text-xl font-semibold">Hi, Naman Malik</div>
                   </div>
-                  <div className='h-[15%] w-full flex flex-col items-center justify-center border-purple-500 '>
-                    <div className='text-slate-800 text-base font-normal'>Your balance is</div>
-                    <div className='text-4xl font-semibold'>₹5000</div>
-                  </div>
-                  <div className='h-[18%] w-full flex justify-evenly items-center '>
-                    <div className='h-[120px] w-[40%] flex flex-col items-left justify-evenly pl-3 border-2'>
-                      <div className='text-4xl flex flex-col justify-center items-center text-center w-10 h-10 bg-[#68a3f0]  rounded-full'>
-                        <p className='mt-[-10px] text-white'>
-                          +
-                        </p>
-                      </div>
-                      <div className='text-xl font-semibold'>
-                        Add Money
-                      </div>
+                  <div className="h-[15%] w-full flex flex-col items-center justify-center border-purple-500 ">
+                    <div className="text-slate-800 text-base font-normal">
+                      Your balance is
                     </div>
-                    <div className='h-[120px] w-[40%] flex items-left justify-evenly flex-col pl-3 border-2'>
-                      <div className='text-4xl font-bold flex flex-col justify-center items-center text-center w-10 h-10 bg-[#68a3f0]  rounded-full'>
-                        <p className='mt-[-10px] text-white'>
-                        ↑
-                        </p>
+                    <div className="text-4xl font-semibold">₹5000</div>
+                  </div>
+                  <div className="h-[25%] w-full flex justify-evenly items-center ">
+                    <div className="h-[100%] w-[40%] flex flex-col items-left justify-evenly pl-3 border-2">
+                      <div className="text-4xl flex flex-col justify-center items-center text-center w-10 h-10 bg-[#68a3f0]  rounded-full">
+                        <p className="mt-[-10px] text-white">+</p>
                       </div>
-                      <div className='text-xl font-semibold'>
-                        Withdraw
+                      <div className="text-xl font-semibold">Add Money</div>
+                    </div>
+                    <div className="h-[100%] w-[40%] flex items-left justify-evenly flex-col pl-3 border-2">
+                      <div className="text-4xl font-bold flex flex-col justify-center items-center text-center w-10 h-10 bg-[#68a3f0]  rounded-full">
+                        <p className="mt-[-10px] text-white">↑</p>
                       </div>
+                      <div className="text-xl font-semibold">Withdraw</div>
                     </div>
                   </div>
-                  <div className='h-[10%] w-full flex items-center justify-center border-purple-500 '>
-                    <button className='h-[80%] w-[95%] rounded-lg border-2 border-red-500'>
-                      Continue
+                  <div className="h-[8.5%] w-full flex items-center justify-center  ">
+                    <button className="h-[100%] w-[85%] rounded-md  bg-blue-600 text-white">
+                      Continue →
                     </button>
                   </div>
                 </div>
               </div>
-              <div className='h-full w-1/2 border-blue-600 border-4'>
-
-              </div>
+              <div className="h-full w-1/2 border-blue-600 border-4"></div>
             </div>
             {/* <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
               <div className="flex items-center p-8 bg-white shadow rounded-lg">
@@ -277,12 +246,11 @@ function Dashboard() {
                 </div>
               </div>
             </section> */}
-
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
