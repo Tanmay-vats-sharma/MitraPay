09cca96c893b7481e534f76b65c3fff8a4bbe4dc
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-11-12 12:50:30 +0530

    Dashboard structure updated

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -3,17 +3,25 @@ import { Navbar1, Navbar2, Navbar3 } from '../Components/Common/Navbar'
 
 function Dashboard() {
   return (
-    <div>
-      <div className="flex bg-gray-100 min-h-screen">
-        {/* Side Navigation Bar */}
-        <Navbar2></Navbar2>
+    {
+      /*<div>*/
+    },
+    {
+      /* <div className="flex bg-gray-100 min-h-screen">*/
+    },
+    {
+      /* Side Navigation Bar */
+    },
+    {
+      /*<Navbar2></Navbar2>
         <div className="flex-grow h-screen text-gray-800 ml-0 sm:ml-0 ">
           <header className="flex justify-between w-[100%] items-center h-16 px-2 sm:px-10 bg-white">
             <div className=" w-[55%]">
               <h1 className="text-4xl font-semibold mb-2">Dashboard</h1>
-            </div>
-
-            <div className="flex flex-shrink-0 items-center width-[100%]">
+            </div> */
+    },
+    {
+      /* <div className="flex flex-shrink-0 items-center width-[100%]">
               <button className="inline-flex wi-[20%] items-center p-2 hover:bg-gray-100 focus:bg-gray-100 rounded-lg">
                 <div className="hidden md:flex md:flex-col md:items-end md:leading-tight">
                   <span className="font-semibold">Grace Simmons</span>
@@ -26,8 +34,10 @@ function Dashboard() {
                     className="h-full w-full object-cover"
                   />
                 </span>
-                <svg
-                  aria-hidden="true"
+                <svg */
+    },
+    {
+      /* aria-hidden="true"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   className="hidden sm:block h-6 w-6 text-gray-300"
@@ -38,8 +48,10 @@ function Dashboard() {
                     clip-rule="evenodd"
                   />
                 </svg>
-              </button>
-              <div className="border-l pl-3 ml-3 space-x-1">
+              </button> */
+    },
+    {
+      /* <div className="border-l pl-3 ml-3 space-x-1">
                 <button className="relative p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-600 focus:bg-gray-100 focus:text-gray-600 rounded-full">
                   <span className="sr-only">Notifications</span>
                   <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-red-500 rounded-full"></span>
@@ -79,31 +91,20 @@ function Dashboard() {
                   Manage Transactions
                 </button>
               </div>
-            </div>
-          </header>
-          <div className="h-[calc(100%-4rem)] flex flex-col justify-evenly">
-            {/* <div className="flex justify-between items-center h-1/6 px-8">
-              <div className="flex flex-wrap items-start justify-end -mb-3">
-                <button className="inline-flex px-5 py-3 text-purple-600 hover:text-purple-700 focus:text-purple-700 hover:bg-purple-100 focus:bg-purple-100 border border-purple-600 rounded-md mb-3">
-                  <svg
-                    aria-hidden="true"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="flex-shrink-0 h-5 w-5 -ml-1 mt-0.5 mr-2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                    />
-                  </svg>
-                  Manage dashboard
-                </button>
-                */}
-            {/* </div> */}
-            <div className="h-[100%] flex ">
+            </div> */
+    },
+    {
+      /* </header> */
+    },
+    {
+      /* <div className="h-[calc(100%-4rem)] flex flex-col justify-evenly">
+       */
+    },
+    {
+      /* </div> */
+    },
+    {
+      /* <div className="h-[100%] flex ">
               <div className="h-full w-[40%] flex justify-center items-center  ">
                 <div className="h-[90%] bg-white w-[75%] flex flex-col justify-evenly items-center rounded-md">
                   <div className="h-[19%] w-full flex flex-col items-center justify-evenly   ">
@@ -115,8 +116,10 @@ function Dashboard() {
                   <div className="h-[15%] w-full flex flex-col items-center justify-center border-purple-500 ">
                     <div className="text-slate-800 text-base font-normal">
                       Your balance is
-                    </div>
-                    <div className="text-4xl font-semibold">₹5000</div>
+                    </div> */
+    },
+    {
+      /* <div className="text-4xl font-semibold">₹5000</div>
                   </div>
                   <div className="h-[20%] w-full flex justify-evenly items-center ">
                     <div className="h-[120%] w-[40%] flex flex-col items-left justify-evenly pl-3 border-2">
@@ -130,16 +133,20 @@ function Dashboard() {
                         <p className="mt-[-10px] text-white">↑</p>
                       </div>
                       <div className="text-xl font-semibold">Withdraw</div>
-                    </div>
-                  </div>
+                    </div> */
+    },
+    {
+      /* </div>
                   <div className="h-[8%] w-full flex items-center justify-center border-purple-500 ">
                     <button className="h-[100%] w-[90%] rounded-md   text-white bg-blue-600  hover:bg-blue-700 focus:bg-blue-700">
                       Continue ⟶
                     </button>
                   </div>
                 </div>
-              </div>
-              <div className="h-full w-[60%]  flex justify-center flex-col items-start">
+              </div> */
+    },
+    {
+      /* <div className="h-full w-[60%]  flex justify-center flex-col items-start">
                 <div className="h-[91%] w-[95%] bg-white flex justify-evenly flex-col items-center rounded-sm p-2">
                   <div className="h-[40%] w-[90%] border-2 bg-gray-100 rounded-sm"></div>
                   <div className="h-[55%] w-[90%] border-2 rounded-sm bg-gray-100"></div>
@@ -148,8 +155,23 @@ function Dashboard() {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div> */
+    },
+    {
+      /*</div>*/
+    },
+    (
+      <>
+        <div className="grid grid-cols-10 grid-rows-8 gap-4   p-3 min-h-screen \
+        ">
+          <div className="border-2 border-red-500 col-span-8 row-span-1  ">1</div>
+          <div className="border-2 border-red-500 col-span-2 row-span-8">2</div>
+          <div className="border-2 border-red-500 col-span-2  row-span-7">3</div>
+          <div className="border-2 border-red-500 col-span-6 row-span-3 "></div>
+          <div className="border-2 border-red-500  col-span-6 row-span-4">4</div>
+        </div>
+      </>
+    )
   );
 }
 
