51a6dd8081aec2d119194e40337e2e1cd5def5cf
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-11-14 00:01:50 +0530

    Balance Div Added to Dashboard

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navbar } from '../Components/Common/Navbar'
-import Loimg from "../assets/loimg.png"
 import { Header } from '../Components/Common/header';
+import { Balance } from '../Components/Common/Balance&History';
 
 function Dashboard() {
   return (
@@ -130,18 +130,18 @@ function Dashboard() {
     },
     (
       <>
-        <div className="grid grid-cols-10 grid-rows-8 gap-3   p-2 min-h-screen \">
+        <div className="grid grid-cols-10 grid-rows-8 gap-3   p-5 min-h-screen \">
           <div className=" col-span-8 row-span-1  flex items-center justify-between  ">
             <Header></Header>
           </div>
-          <div className="border-2 border-red-500 col-span-2 row-span-8"></div>
+          <div className="bg-blue-50 rounded-lg col-span-2 row-span-8 p-1">
+            <Balance></Balance>
+          </div>
           <div className=" col-span-2  row-span-7  p-1 ">
             <Navbar></Navbar>
           </div>
-          <div className="border-2 border-red-500 col-span-6 row-span-3 "></div>
-          <div className="border-2 border-red-500  col-span-6 row-span-4">
-            4
-          </div>
+          <div className="bg-blue-50 rounded-md col-span-6 row-span-3 "></div>
+          <div className="bg-blue-50 rounded-md  col-span-6 row-span-4">4</div>
         </div>
       </>
     )
