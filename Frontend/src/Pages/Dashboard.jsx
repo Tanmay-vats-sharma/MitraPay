5759795408866d5d4f729d8a2671feb72d90bd09
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-11-14 23:46:44 +0530

    Dashboard colour theme changed

--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -6,19 +6,19 @@ import { Balance } from '../Components/Common/Balance&History';
 function Dashboard() {
   return (
     <>
-      <div className="grid grid-cols-10 grid-rows-8 gap-3   p-5 min-h-screen \">
-        <div className=" col-span-8 row-span-1  flex items-center justify-between  ">
+      <div className="grid grid-cols-10 grid-rows-8 gap-3 bg-blue-50  p-5 min-h-screen \">
+        <div className=" col-span-8 row-span-1 bg-white shadow-sm  flex items-center justify-between rounded-lg ">
           <Header></Header>
         </div>
-        <div className=" bg-[#EFF2FB] rounded-lg col-span-2 row-span-8 p-1">
+        <div className=" bg-white shadow-lg rounded-lg col-span-2 row-span-8 p-4">
           <Balance></Balance>
         </div>
-        <div className=" col-span-2  row-span-7  p-1 ">
+        <div className=" col-span-2 bg-white shadow-lg rounded-lg row-span-7  p-4 ">
           <Navbar></Navbar>
         </div>
-        <div className="bg-[#EFF2FB] rounded-md col-span-6 row-span-3 "></div>
-        <div className="bg-[#EFF2FB]   rounded-md  col-span-6 row-span-4">
-          4
+        <div className="bg-white shadow-current rounded-lg  col-span-6 row-span-3 "></div>
+        <div className="bg-white shadow-lg  border-[1px]  rounded-lg  col-span-6 row-span-4">
+          
         </div>
       </div>
     </>
