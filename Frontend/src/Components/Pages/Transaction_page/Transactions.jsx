f7611513f5f3d6510f5ca95ef5a6f0aaef9020e5
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-12-08 23:31:17 +0530

    Minor changes

--- Frontend/src/Components/Pages/Transaction_page/Transactions.jsx
+++ Frontend/src/Components/Pages/Transaction_page/Transactions.jsx
@@ -5,13 +5,15 @@ export function Transactions() {
     <>
       <div className="bg-white shadow-lg justify-evenly overflow-y-hidden border-[1px] flex flex-col items-center rounded-lg py-3 col-span-6 row-span-7">
         
-          <div className="w-[95%] h-[8%] flex border-b-2 border-gray-400">
-            <div className="w-[90%]  flex justify-start text-xl font-semibold ">
+          <div className="w-[95%] h-[8%] flex justify-between border-b-2 border-gray-400">
+            <div className="w-[auto]  flex  text-xl font-semibold ">
               <p>Transaction History</p>
             </div>
-            
+          <div className="w-[40%] border-2">
+            <button></button>  
+            <button></button>  
           </div>
-          {/* <hr className="w-[95%] border-t-2 border-gray-400 items-center mb-[10px]" /> */}
+          </div>
           <div className="w-[95%] h-[420px] hide-scrollbar flex flex-col items-center  overflow-y-scroll justify-start p-1 bg-gray-50 rounded-md">
             <Transaction></Transaction>
             <Transaction></Transaction>
