7375cbc02bd305a7e2903aaf3c06010fa227de45
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-12-09 11:29:27 +0530

    Transaction page  Completed

--- Frontend/src/Components/Common/Navbar.jsx
+++ Frontend/src/Components/Common/Navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar() {
           <ul className="flex flex-col  space-y-2  h-[auto] w-[100%] ">
             <li>
               <a
-                href="/dashboard"
+                href="/Dashboard"
                 className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
               >
                 <span className="inline-flex justify-center items-center ml-4">
--- Frontend/src/Components/Pages/Transaction_page/Transactions.jsx
+++ Frontend/src/Components/Pages/Transaction_page/Transactions.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { Transaction } from "../../Common/Transaction";
+import Add_money_button from "../../../Assets/Add_money_button.png"
 export function Transactions() {
   return (
     <>
       <div className="bg-white shadow-lg justify-evenly overflow-y-hidden border-[1px] flex flex-col items-center rounded-lg py-3 col-span-6 row-span-7">
-        
-          <div className="w-[95%] h-[8%] flex justify-between border-b-2 border-gray-400">
-            <div className="w-[auto]  flex  text-xl font-semibold ">
-              <p>Transaction History</p>
-            </div>
-          <div className="w-[40%] border-2">
-            <button></button>  
-            <button></button>  
+        <div className="w-[95%] h-[8%] flex justify-between border-b-2 p-1 border-gray-400">
+          <div className="w-[auto]  flex  text-xl font-semibold ">
+            <p>Transaction History</p>
           </div>
+          <div className="w-[45%] h-[100%] flex justify-around">
+            <button className="bg-blue-600 w-[40%] flex rounded-md  justify-center px-1 items-center hover:bg-blue-800 ">
+              <div className="w-[20%] mb-[1px] text-white text-[25px] font-mono ">
+                +
+              </div>
+              <div className="w-[60%]   text-[0.9rem] text-white font-mono">
+                Add Money
+              </div>
+            </button>
+            <button className="bg-blue-600 w-[40%] flex rounded-md border-[1px] border-gray-400 justify-center p-1 items-center hover:bg-blue-800">
+              <div className="w-[20%]  text-white text-[20px] font-mono">
+                ↗
+              </div>
+              <div className="w-[60%]   text-[0.9rem] tracking-tighter text-white font-mono">
+                Send Money
+              </div>
+            </button>
           </div>
-          <div className="w-[95%] h-[420px] hide-scrollbar flex flex-col items-center  overflow-y-scroll justify-start p-1 bg-gray-50 rounded-md">
-            <Transaction></Transaction>
-            <Transaction></Transaction>
-            <Transaction></Transaction>
-            
-          </div>
-        
+        </div>
+        <div className="w-[95%] h-[420px] hide-scrollbar flex flex-col items-center  overflow-y-scroll justify-start p-1 bg-gray-50 rounded-md">
+          <Transaction></Transaction>
+          <Transaction></Transaction>
+          <Transaction></Transaction>
+        </div>
       </div>
     </>
   );
