52435eec26f5549a63420e8c5115975f771c3b25
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-12-10 08:07:25 +0530

    Profile Page Personal Info div Added

--- Frontend/src/Components/Pages/Profile_page/profile.jsx
+++ Frontend/src/Components/Pages/Profile_page/profile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Carousel } from "./Carousel";
 
-export  function Profile(){
+export function Profile() {
   return (
     <>
       <div className="bg-white  rounded-lg flex flex-col justify-evenly shadow-lg items-center col-span-6 row-span-7 p-1">
@@ -17,14 +17,33 @@ export  function Profile(){
             </button>
           </div>
           <div className="w-[100%] h-[75%] flex items-center justify-center ">
-             <Carousel></Carousel>
+            <Carousel></Carousel>
           </div>
         </div>
-        <div className="w-[100%] h-[58%] ">
-          <div className="flex justify-start w-[100%] h-[15%]  text-2xl font-semibold tracking-tighter px-4">
+        <div className="w-[100%] h-[58%] px-2">
+          <div className="flex justify-start w-[100%] h-[14%]  text-2xl font-semibold tracking-tighter px-3">
             <p>Personal Information</p>
           </div>
-          <div className=""></div>
+          <div className="flex flex-col justify-evenly w-[100%] h-[86%] border-2 border-red-400  ">
+            <div className="h-[20%] border-2 border-red-700 flex ">
+              <div className="w-[50%] h-[100%] flex flex-col justify-evenly" >
+                <div className="h-[50%] w-[100%] text-lg font-semibold flex justify-start px-2 items-center">
+                  <p>First Name</p>
+                </div>
+                <div className="h-[50%] w-[100%] flex justify-start px-2">
+                   <input type="text" placeholder="Tanmay Sharma" className="h-[100%] px-2"/>
+                </div>
+              </div>
+              <div className="w-[50%] h-[100%]">
+                <div className=""></div>
+                <div className=""></div>
+              </div>
+            </div>
+            <div className="h-[20%] "></div>
+            <div className="h-[20%] "></div>
+            <div className="h-[20%] "></div>
+            <div className="h-[20%] "></div>
+          </div>
         </div>
       </div>
     </>
