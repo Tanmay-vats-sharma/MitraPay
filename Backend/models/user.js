f7d49d3f4ee70c86c4eb02514d61568810cb2558
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-12-11 14:28:13 +0530

    User model created

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -10,21 +10,60 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 
-let userschma = mongoose.Schema({
+let userschma = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    profile_pic:{
+      type: String,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /.+\@.+\..+/ 
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+\@.+\..+/,
     },
     password: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+    Address: {
+      type: String,
+    },
+    Phone_no: {
+      type: String,
+      required: true,
+    },
+    wallet: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "wallet",
+      required: true,
+    },
+    contacts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "contact",
+        unique: true,
+      },
+    ],
+    gullak: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "gullak",
+        unique: true,
+      },
+    ],
+    transactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "transaction",
+        unique:true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("user", userschma);
