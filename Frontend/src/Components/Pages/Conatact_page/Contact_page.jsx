8ac4c9c3625ca3e03e105d78bea768d4834b630a
Tanmay Sharma <tanmaysharmats36@gmail.com>
2024-12-11 14:08:40 +0530

    Conatct Page cpleted

--- Frontend/src/Components/Pages/Conatact_page/Contact_page.jsx
+++ Frontend/src/Components/Pages/Conatact_page/Contact_page.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { Contact } from "./Contact";
 import { Message } from "./Message";
 import { Chat } from "./Chat";
+import send from "../../../assets/send.png"
 
 export function Contact_page() {
 
@@ -10,48 +11,19 @@ export function Contact_page() {
       <div className="bg-white  rounded-lg flex flex-col justify-evenly shadow-lg items-center col-span-6 row-span-7 ">
         <Contact></Contact>
         <Chat></Chat>
-        <div className="w-[100%] h-[12%]  border-[1px] border-gray-300 rounded-b-lg bg-gray-100 flex justify-around items-center">
-          <div className="w-[5%] h-[80%]  flex justify-center items-center">
-            <button class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                class="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17 10l4-4m0 0l-4-4m4 4H7m10 4l-4 4m0 0l4 4m-4-4H7"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div className="w-[80%] h-[80%]  flex justify-center items-center">
+        <div className="w-[100%] h-[12%]  border-[1px] border-gray-300 rounded-b-lg bg-gray-100 flex justify-evenly items-center">
+          <div className="w-[90%] h-[85%]  flex justify-center items-center">
             <input
               type="text"
               placeholder="Type your Message here..."
-              className="w-[100%] h-[80%] p-2 rounded-2xl  outline-none border-[1px] border-gray-200"
+              className="w-[100%] h-[85%] px-4 rounded-2xl  outline-none border-[1px] border-gray-400"
             />
           </div>
-          <div className="w-[5%] h-[80%] border-2 flex justify-center items-center">
-            <button class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                class="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17 10l4-4m0 0l-4-4m4 4H7m10 4l-4 4m0 0l4 4m-4-4H7"
-                ></path>
-              </svg>
+          <div className="w-[5%] h-[60%] flex justify-center items-center p-1">
+            <button class="  focus:outline-none  flex items-center hover:-rotate-45 delay-150">
+              <div className="w-[35px] h-[20px] flex justify-center items-center">
+                <img src={send} alt="" className="w-[45px] h-[40px]" />
+              </div>
             </button>
           </div>
         </div>
